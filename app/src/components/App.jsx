e6f9472b20421d4e4d35d6e7d19f076bb3958bf1
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-05-07 12:28:49 +0200

    Template to start project

---
+++ app/src/components/App.jsx
@@ -0,0 +1,11 @@
+import Question from "./question"
+
+const App = () => {
+    return (
+        <main className="container">
+            <Question />
+        </main>
+    )
+}
+
+export default App
