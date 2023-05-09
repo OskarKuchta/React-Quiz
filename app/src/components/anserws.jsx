34e2d08d4b7685be956eac36981f3d67383e60ef
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-05-09 17:47:51 +0200

    Add dynamically options to question

--- app/src/components/anserws.jsx
+++ app/src/components/anserws.jsx
@@ -1,29 +1,40 @@
 import { useState } from "react";
 import Submit from "./submit";
+import questions from "./questions";
 
 const Answer = () => {
   const [value, setValue] = useState("");
+  const [message, setMessage] = useState("");
   const getValue = (event) => {
     setValue(event.target.innerHTML);
   };
   const checkAnswer = () => {
-    console.log(value)
-  }
+    console.log(value);
+    console.log(message);
+    if (value.trim() == "") {
+      return;
+    }
+    if (value == "Paris") {
+      setMessage("Correct answer");
+    }
+    setMessage("Incorrect answer");
+  };
   return (
     <>
       <button onClick={getValue} className="btn option">
-        1
+        {questions[0].options[0]}
       </button>
       <button onClick={getValue} className="btn option">
-        2
+        {questions[0].options[1]}
       </button>
       <button onClick={getValue} className="btn option">
-        3
+        {questions[0].options[2]}
       </button>
       <button onClick={getValue} className="btn option">
-        4
+        {questions[0].options[3]}
       </button>
-      <Submit onClick={checkAnswer}/>
+      <Submit onClick={checkAnswer} />
+      {message ? <p>{message}</p> : ""}
     </>
   );
 };
