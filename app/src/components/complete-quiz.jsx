67703e0c66638024cfebd1ec342ea3e2d69676bd
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-05-12 17:53:15 +0200

    Fix bug with double message, missed once check statement

--- app/src/components/complete-quiz.jsx
+++ app/src/components/complete-quiz.jsx
@@ -9,10 +9,10 @@ const Finished = (props) => {
         You complete quiz with {props.correct} / {props.all} ( {percent}% )
         correct answers!
       </h2>
-      {percent < 40 ? (
+      {percent <= 40 ? (
         <h2 className="header">Don't give up, next time will be better!</h2>
       ) : null}
-      {percent <= 66 ? <h2 className="header">Nice score, your knowledge is great</h2> : null}
+      {percent <= 66  && percent > 40? <h2 className="header">Nice score, your knowledge is great</h2> : null}
       {percent > 66 ? (
         <h2 className="header">Your knowledge is awesome, good job!</h2>
       ) : null}
